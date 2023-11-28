6c7af05b39a5294bd77a25f7541c6e2be647e2e1
Tripuraneni-Harsha <70709706+Tripuraneni-Harsha@users.noreply.github.com>
2023-11-28 16:32:34 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='Project 3',
+    version='0.2.1',
+    description='Project-3 Technical analysis',
+    author='Venkat Sai Sri Harsha Tripuraneni',
+    author_email='vtripura@mail.yu.edu',
+    license='MIT',
+    url='https://github.com/Tripuraneni-Harsha/project-3',
+    packages=['Project 3'],
+    install_requires=[
+        'matplotlib>=3.7.1',
+        'numpy>=1.24.3',
+        'pandas>=1.5.3',
+        'seaborn>=0.12.2',
+    ],
+)
